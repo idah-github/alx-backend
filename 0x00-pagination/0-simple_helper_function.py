9cf89c30dd7ba410ec139b43b436bf77f26144e4
idah-github <guantaiidah@gmail.com>
2024-08-07 04:58:00 +0000

    debug

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -9,5 +9,5 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     return tuple size 2 with statrt and end index
     """
     start_index = (page - 1) * page_size
-    end_index =page *page_size
+    end_index =page * page_size
     return (start_index, end_index)
